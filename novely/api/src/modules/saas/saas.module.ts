bff30a464b9264dd9ae362961f6a22ac4a887bdb
André Lucas <apglu.26@gmail.com>
2026-04-21 11:20:27 -0300

    Reapply "feat(subscription + customers)"
    This reverts commit a927a7d0d508ab971a55c594b711c5183b4ca4fa.

--- novely/api/src/modules/saas/saas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SaasService } from './saas.service';
-import { SaasController } from './saas.controller';
-
-@Module({
-  controllers: [SaasController],
-  providers: [SaasService],
-})
-export class SaasModule {}
